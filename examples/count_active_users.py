703e9ff42141ad73867c7adf088e4fed6241c54a
Alex Manning <alexander.manning@stfc.ac.uk>
2024-08-20 08:47:05 +0100

    Add example to get active users.

---
+++ examples/count_active_users.py
@@ -0,0 +1,20 @@
+"""Number of users who have an active jasmin_accounts_portal account.."""
+
+import json
+import os
+
+import jasmin_account_api_client as jclient
+import jasmin_account_api_client.api.users as jusers
+
+client = jclient.AuthenticatedClient("https://accounts.jasmin.ac.uk")
+client.client_credentials_flow(
+    client_id=os.environ["JASMIN_CLIENT_ID"],
+    client_secret=os.environ["JASMIN_CLIENT_SECRET"],
+    scopes=[
+        "jasmin.auth.users.all:read",  # users_retrieve and users_list
+    ],
+)
+
+active_users = jusers.users_list.sync(client=client, is_active=False)
+
+print(len(active_users))
